1eba125d66ecbe84c1dea7a04ad8cf0bbd6904cc
Nerogar <nerogar@arcor.de>
2023-07-18 18:31:59 +0200

    fix exception when starting an embedding training

--- modules/modelLoader/StableDiffusionEmbeddingModelLoader.py
+++ modules/modelLoader/StableDiffusionEmbeddingModelLoader.py
@@ -101,7 +101,7 @@ class StableDiffusionEmbeddingModelLoader(BaseModelLoader):
             extra_model_name: str | None
     ) -> StableDiffusionModel | None:
         base_model_loader = StableDiffusionModelLoader()
-        model = base_model_loader.load(model_type, base_model_name, None)
+        model = base_model_loader.load(model_type, weight_dtype, base_model_name, None)
 
         embedding_loaded = self.__load_internal(model, extra_model_name)
 
