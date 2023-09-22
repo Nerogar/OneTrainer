b6cc3ed8a2e3461adfba42144b512f94fb2b637a
Nerogar <nerogar@arcor.de>
2023-09-22 21:14:37 +0200

    replace rembg dependency with custom code to reduce dependency hell

--- modules/module/RembgModel.py
+++ modules/module/RembgModel.py
@@ -1,7 +1,12 @@
-from typing import Optional
+import os
+from typing import Optional, Tuple
 
-import rembg
+import numpy as np
+import onnxruntime
+import pooch
 import torch
+from PIL import Image
+from numpy import ndarray
 from torch import Tensor, nn
 from torchvision.transforms import functional, transforms
 
@@ -13,7 +18,7 @@ class RembgModel(BaseImageMaskModel):
         self.device = device
         self.dtype = dtype
 
-        self.rembg_session = rembg.new_session()
+        self.model = self.__load_model()
 
         self.smoothing_kernel_radius = None
         self.smoothing_kernel = self.__create_average_kernel(self.smoothing_kernel_radius)
@@ -24,6 +29,20 @@ class RembgModel(BaseImageMaskModel):
         self.image2Tensor = transforms.Compose([
             transforms.ToTensor(),
         ])
+
+    def __load_model(self) -> onnxruntime.InferenceSession:
+        filename = "u2net.onnx"
+        path = os.path.join("external", "models", "rembg")
+
+        pooch.retrieve(
+            "https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2net.onnx",
+            "md5:60024c5c889badc19c04ad937298a77b",
+            fname=filename,
+            path=path,
+            progressbar=True,
+        )
+
+        return onnxruntime.InferenceSession(os.path.join(path, filename))
 
     def __create_average_kernel(self, kernel_radius: Optional[int]):
         if kernel_radius is None:
@@ -55,6 +74,27 @@ class RembgModel(BaseImageMaskModel):
 
         return mask
 
+    def __normalize(
+            self,
+            img: Image.Image,
+            mean: Tuple[float, float, float],
+            std: Tuple[float, float, float],
+            size: Tuple[int, int],
+    ) -> ndarray:
+        im = img.resize(size, Image.LANCZOS)
+
+        im_ary = np.array(im)
+        im_ary = im_ary / np.max(im_ary)
+
+        tmpImg = np.zeros((im_ary.shape[0], im_ary.shape[1], 3))
+        tmpImg[:, :, 0] = (im_ary[:, :, 0] - mean[0]) / std[0]
+        tmpImg[:, :, 1] = (im_ary[:, :, 1] - mean[1]) / std[1]
+        tmpImg[:, :, 2] = (im_ary[:, :, 2] - mean[2]) / std[2]
+
+        tmpImg = tmpImg.transpose((2, 0, 1))
+
+        return np.expand_dims(tmpImg, 0).astype(np.float32)
+
     def mask_image(
             self,
             filename: str,
@@ -77,8 +117,27 @@ class RembgModel(BaseImageMaskModel):
             self.expand_kernel = self.__create_average_kernel(expand_pixels)
             self.expand_kernel_radius = expand_pixels
 
-        output = rembg.remove(mask_sample.get_image(), only_mask=True, session=self.rembg_session).convert("RGB")
-        output = self.image2Tensor(output).to(self.device)
+        image = mask_sample.get_image()
+
+        normalized_image = self.__normalize(
+            image,
+            (0.485, 0.456, 0.406),
+            (0.229, 0.224, 0.225),
+            (320, 320)
+        )
+
+        input_name = self.model.get_inputs()[0].name
+        mask = self.model.run(None, {input_name: normalized_image})
+
+        mask = mask[0][:, 0, :, :]
+
+        ma = np.max(mask)
+        mi = np.min(mask)
+
+        mask = (mask - mi) / (ma - mi)
+        mask = np.squeeze(mask)
+
+        output = torch.from_numpy(mask).to(self.device)
 
         predicted_mask = self.__process_mask(output, mask_sample.height, mask_sample.width, threshold)
 
