3f3acd613de25d91dd43f5b136e3fbda8faaf3b4
Nerogar <nerogar@arcor.de>
2024-03-03 00:14:23 +0100

    fix loading of samples and concepts when loading a packed config file

--- modules/util/config/BaseConfig.py
+++ modules/util/config/BaseConfig.py
@@ -85,7 +85,8 @@ class BaseConfig:
                     if len(get_args(self.types[name])) > 0 and issubclass(get_args(self.types[name])[0], BaseConfig):
                         list_type = get_args(self.types[name])[0]
                         if data[name] is not None:
-                            old_value = getattr(self, name) if hasattr(self, name) else []
+                            old_value = \
+                                (getattr(self, name)) if hasattr(self, name) and getattr(self, name) is not None else []
                             value = []
                             for i in range(max(len(old_value), len(data[name]))):
                                 if i < len(old_value) and i < len(data[name]):
