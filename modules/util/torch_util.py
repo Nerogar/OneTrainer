81606a805b2a03d594a7df7f2eb118cb56c9fd0b
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2024-03-13 09:28:37 +0100

    Fix GC to not use too much memory.

--- modules/util/torch_util.py
+++ modules/util/torch_util.py
@@ -4,11 +4,15 @@ import torch
 
 
 def torch_gc():
-    gc.collect()
     if torch.cuda.is_available():
         torch.cuda.synchronize()
+    if torch.backends.mps.is_available():
+        torch.mps.synchronize()
+    
+    gc.collect()
+    
+    if torch.cuda.is_available():
         torch.cuda.empty_cache()
     
     if torch.backends.mps.is_available():
-        torch.mps.synchronize()
         torch.mps.empty_cache()
