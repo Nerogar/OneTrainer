5fec095c5c62118d1ca6f3c5ef849d66d0b69b63
Morgon Kanter <mx@users.noreply.github.com>
2024-08-06 13:23:33 -0400

    Propagate the train/eval state to lora modules. (#424)

--- modules/module/LoRAModule.py
+++ modules/module/LoRAModule.py
@@ -15,6 +15,8 @@ from modules.util.enum.ModelType import PeftType
 class PeftBase(nn.Module):
     is_applied: bool
     orig_forward: Any | None
+    orig_eval: Any | None
+    orig_train: Any | None
     _orig_module: list[nn.Module] | None  # list prevents it from registering
     prefix: str
     layer_kwargs: dict  # Applied during the forward op() call.
@@ -46,14 +48,28 @@ class PeftBase(nn.Module):
     def hook_to_module(self):
         if not self.is_applied:
             self.orig_forward = self.orig_module.forward
+            self.orig_train = self.orig_module.train
+            self.orig_eval = self.orig_module.eval
             self.orig_module.forward = self.forward
+            self.orig_module.train = self._wrap_train
+            self.orig_module.eval = self._wrap_eval
             self.is_applied = True
 
     def remove_hook_from_module(self):
         assert self.orig_forward is not None
         if self.is_applied:
             self.orig_module.forward = self.orig_forward
+            self.orig_module.train = self.orig_train
+            self.orig_module.eval = self.orig_eval
             self.is_applied = False
+
+    def _wrap_train(self, mode=True):
+        self.orig_train(mode)
+        self.train(mode)
+
+    def _wrap_eval(self):
+        self.orig_eval()
+        self.eval()
 
     def make_weight(self, A: Tensor, B: Tensor):
         """Layer-type-independent way of creating a weight matrix from LoRA A/B.
@@ -301,11 +317,8 @@ class LoRAModule(PeftBase):
     def forward(self, x, *args, **kwargs):
         self.check_initialized()
 
-        if self.orig_module.training:
-            ld = self.lora_up(self.dropout(self.lora_down(x)))
-            return self.orig_forward(x) + ld * (self.alpha / self.rank)
-
-        return self.orig_forward(x) + self.lora_up(self.lora_down(x)) * (self.alpha / self.rank)
+        ld = self.lora_up(self.dropout(self.lora_down(x)))
+        return self.orig_forward(x) + ld * (self.alpha / self.rank)
 
     def apply_to_module(self):
         # TODO
