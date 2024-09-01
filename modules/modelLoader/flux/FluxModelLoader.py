aa18c65a7450e2c893cf2f6db19685ef5645fea4
Nerogar <nerogar@arcor.de>
2024-09-01 23:24:52 +0200

    move model loading functions out of FluxModelLoader

--- modules/modelLoader/flux/FluxModelLoader.py
+++ modules/modelLoader/flux/FluxModelLoader.py
@@ -1,15 +1,12 @@
-import json
 import os
 import traceback
 
 from modules.model.FluxModel import FluxModel
-from modules.util.enum.DataType import DataType
+from modules.modelLoader.mixin.HFModelLoaderMixin import HFModelLoaderMixin
 from modules.util.enum.ModelType import ModelType
 from modules.util.ModelNames import ModelNames
 from modules.util.ModelWeightDtypes import ModelWeightDtypes
-from modules.util.quantization_util import replace_linear_with_int8_layers, replace_linear_with_nf4_layers
-
-from torch import nn
+from modules.util.quantization_util import replace_linear_with_nf4_layers
 
 from diffusers import (
     AutoencoderKL,
@@ -19,158 +16,12 @@ from diffusers import (
 )
 from transformers import CLIPTextModel, CLIPTokenizer, T5EncoderModel, T5Tokenizer
 
-import accelerate
-import huggingface_hub
-from huggingface_hub.utils import EntryNotFoundError
-from safetensors.torch import load_file
 
-
-class FluxModelLoader:
+class FluxModelLoader(
+    HFModelLoaderMixin,
+):
     def __init__(self):
         super(FluxModelLoader, self).__init__()
-
-    def __load_sub_module(
-            self,
-            sub_module: nn.Module,
-            dtype: DataType,
-            keep_in_fp32_modules: list[str] | None,
-            pretrained_model_name_or_path: str,
-            subfolder: str,
-            model_filename: str,
-            shard_index_filename: str,
-    ):
-        with accelerate.init_empty_weights():
-            if dtype.quantize_nf4():
-                replace_linear_with_nf4_layers(sub_module, keep_in_fp32_modules)
-            elif dtype.quantize_int8():
-                replace_linear_with_int8_layers(sub_module, keep_in_fp32_modules)
-
-        is_local = os.path.isdir(pretrained_model_name_or_path)
-
-        if is_local:
-            full_shard_index_filename = os.path.join(pretrained_model_name_or_path, subfolder, shard_index_filename)
-            if not os.path.isfile(full_shard_index_filename):
-                full_shard_index_filename = None
-        else:
-            try:
-                full_shard_index_filename = huggingface_hub.hf_hub_download(
-                    repo_id=pretrained_model_name_or_path,
-                    subfolder=subfolder,
-                    filename=shard_index_filename,
-                )
-            except EntryNotFoundError:
-                full_shard_index_filename = None
-
-        is_sharded = full_shard_index_filename is not None
-
-        if is_sharded:
-            with open(full_shard_index_filename, "r") as f:
-                index_file = json.loads(f.read())
-                filenames = sorted(set(index_file["weight_map"].values()))
-        else:
-            filenames = [model_filename]
-
-        if is_local:
-            full_filenames = [os.path.join(pretrained_model_name_or_path, subfolder, f) for f in filenames]
-        else:
-            full_filenames = [huggingface_hub.hf_hub_download(
-                repo_id=pretrained_model_name_or_path,
-                subfolder=subfolder,
-                filename=f,
-            ) for f in filenames]
-
-        state_dict = {}
-
-        for f in full_filenames:
-            state_dict |= load_file(f)
-
-        for key, value in state_dict.items():
-            module = sub_module
-            tensor_name = key
-            key_splits = tensor_name.split(".")
-            for split in key_splits[:-1]:
-                module = getattr(module, split)
-            tensor_name = key_splits[-1]
-
-            is_buffer = tensor_name in module._buffers
-            old_value = module._buffers[tensor_name] if is_buffer else module._parameters[tensor_name]
-
-            old_type = type(old_value)
-            value = value.to(dtype=dtype.torch_dtype())
-            new_value = old_type(value)
-
-            if is_buffer:
-                module._buffers[tensor_name] = new_value
-            else:
-                module._parameters[tensor_name] = new_value
-
-        del state_dict
-
-        return sub_module
-
-    def load_transformers_sub_module(
-            self,
-            module_type,
-            dtype: DataType,
-            pretrained_model_name_or_path: str,
-            subfolder: str,
-    ):
-        user_agent = {
-            "file_type": "model",
-            "framework": "pytorch",
-        }
-        config, model_kwargs = module_type.config_class.from_pretrained(
-            pretrained_model_name_or_path,
-            subfolder=subfolder,
-            return_unused_kwargs=True,
-            return_commit_hash=True,
-            user_agent=user_agent,
-        )
-
-        with accelerate.init_empty_weights():
-            sub_module = module_type(config)
-
-        return self.__load_sub_module(
-            sub_module=sub_module,
-            dtype=dtype,
-            keep_in_fp32_modules=module_type._keep_in_fp32_modules,
-            pretrained_model_name_or_path=pretrained_model_name_or_path,
-            subfolder=subfolder,
-            model_filename="model.safetensors",
-            shard_index_filename="model.safetensors.index.json",
-        )
-
-    def load_diffusers_sub_module(
-            self,
-            module_type,
-            dtype: DataType,
-            pretrained_model_name_or_path: str,
-            subfolder: str,
-    ):
-        user_agent = {
-            "file_type": "model",
-            "framework": "pytorch",
-        }
-        config, unused_kwargs, commit_hash = module_type.load_config(
-            pretrained_model_name_or_path,
-            subfolder=subfolder,
-            return_unused_kwargs=True,
-            return_commit_hash=True,
-            user_agent=user_agent,
-        )
-
-        with accelerate.init_empty_weights():
-            sub_module = module_type.from_config(config)
-
-        return self.__load_sub_module(
-            sub_module=sub_module,
-            dtype=dtype,
-            keep_in_fp32_modules=None,
-            pretrained_model_name_or_path=pretrained_model_name_or_path,
-            subfolder=subfolder,
-            model_filename="diffusion_pytorch_model.safetensors",
-            shard_index_filename="diffusion_pytorch_model.safetensors.index.json",
-        )
 
     def __load_internal(
             self,
@@ -222,10 +73,11 @@ class FluxModelLoader:
         )
 
         if include_text_encoder_1:
-            text_encoder_1 = CLIPTextModel.from_pretrained(
+            text_encoder_1 = self._load_transformers_sub_module(
+                CLIPTextModel,
+                weight_dtypes.text_encoder,
                 base_model_name,
-                subfolder="text_encoder",
-                torch_dtype=weight_dtypes.text_encoder.torch_dtype(),
+                "text_encoder",
             )
             text_encoder_1.text_model.embeddings.to(dtype=weight_dtypes.text_encoder.torch_dtype(
                 supports_quantization=False))
@@ -233,7 +85,7 @@ class FluxModelLoader:
             text_encoder_1 = None
 
         if include_text_encoder_2:
-            text_encoder_2 = self.load_transformers_sub_module(
+            text_encoder_2 = self._load_transformers_sub_module(
                 T5EncoderModel,
                 weight_dtypes.text_encoder_2,
                 base_model_name,
@@ -245,18 +97,20 @@ class FluxModelLoader:
             text_encoder_2 = None
 
         if vae_model_name:
-            vae = AutoencoderKL.from_pretrained(
+            vae = self._load_diffusers_sub_module(
+                AutoencoderKL,
+                weight_dtypes.vae,
                 vae_model_name,
-                torch_dtype=weight_dtypes.vae.torch_dtype(),
             )
         else:
-            vae = AutoencoderKL.from_pretrained(
+            vae = self._load_diffusers_sub_module(
+                AutoencoderKL,
+                weight_dtypes.vae,
                 base_model_name,
-                subfolder="vae",
-                torch_dtype=weight_dtypes.vae.torch_dtype(),
+                "vae",
             )
 
-        transformer = self.load_diffusers_sub_module(
+        transformer = self._load_diffusers_sub_module(
             FluxTransformer2DModel,
             weight_dtypes.prior,
             base_model_name,
