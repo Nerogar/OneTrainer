4bd03a6cb1202118c79b3c2636e4b5ad0e6b9982
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2024-09-05 08:25:12 +0200

    Fix Flux batching warning.

--- modules/model/FluxModel.py
+++ modules/model/FluxModel.py
@@ -291,16 +291,9 @@ class FluxModel(BaseModel):
         return text_encoder_2_output, pooled_text_encoder_1_output
 
     def prepare_latent_image_ids(self, batch_size, height, width, device, dtype):
-        latent_image_ids = torch.zeros(height // 2, width // 2, 3)
-        latent_image_ids[..., 1] = latent_image_ids[..., 1] + torch.arange(height // 2)[:, None]
-        latent_image_ids[..., 2] = latent_image_ids[..., 2] + torch.arange(width // 2)[None, :]
-
-        latent_image_id_height, latent_image_id_width, latent_image_id_channels = latent_image_ids.shape
-
-        latent_image_ids = latent_image_ids[None, :].repeat(batch_size, 1, 1, 1)
-        latent_image_ids = latent_image_ids.reshape(
-            batch_size, latent_image_id_height * latent_image_id_width, latent_image_id_channels
-        )
+        latent_image_ids = torch.empty(height // 2, width // 2, 3)
+        latent_image_ids[..., 1] = torch.arange(height // 2)[:, None]
+        latent_image_ids[..., 2] = torch.arange(width // 2)[None, :]
 
         return latent_image_ids.to(device=device, dtype=dtype)
 
