b79abcf8f0a4880f15e0e57e840c441997b713c2
Lolzen <funer@gmx.at>
2024-09-17 13:22:49 +0200

    update.sh: add missing git pull & --force reinstall for python venv

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 source ./linux-env-vars.sh
 
-export linux_cmd_python_venv = '${python_cmd} -m pip install -r requirements-global.txt -r $PLATFORM_REQS'
+export linux_cmd_python_venv = 'git pull && ${python_cmd} -m pip install -r requirements-global.txt -r $PLATFORM_REQS --force-reinstall'
 export linux_cmd_conda_venv = 'git pull; ${python_cmd} -m pip install -r requirements-global.txt -r $PLATFORM_REQS" --force-reinstall)'
 
 /bin/bash ./linux-python-env.sh
