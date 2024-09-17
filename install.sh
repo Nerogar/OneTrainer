7f325374141760a254f9bb2e9e12ead1ea22dd6d
Lolzen <funer@gmx.at>
2024-09-17 14:28:03 +0200

    use linux platform check once on install and let the user decide; store the value in linux-platform for using it when updating

--- install.sh
+++ install.sh
@@ -2,7 +2,58 @@
 
 source ./linux-env-vars.sh
 
-export linux_cmd_python_venv = '${python_cmd} -m pip install -r requirements-global.txt -r $PLATFORM_REQS'
-export linux_cmd_conda_venv = '${python_cmd} -m pip install -r requirements-global.txt -r $PLATFORM_REQS'
+ask() {
+    local prompt default reply
+
+    if [[ ${2:-} = 'Y' ]]; then
+        prompt='Y/n'
+        default='Y'
+    elif [[ ${2:-} = 'N' ]]; then
+        prompt='y/N'
+        default='N'
+    else
+        prompt='y/n'
+        default=''
+    fi
+
+    while true; do
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read -r reply </dev/tty
+
+        # Default?
+        if [[ -z $reply ]]; then
+            reply=$default
+        fi
+
+        # Check if the reply is valid
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
+
+#clear linux-platform (used  to store the selected platform-requirements, so we don't have to confirm on every update)
+> linux-platform
+
+echo ""
+echo "Platform requirements diverge slightly sepending on you GPU."Would you want to use the detected platform Requirements? (echo $PLATFORM_REQS)
+if ask "Use $PLATFORM_REQS? default: Yes" Y; then
+    echo "Using $PLATFORM_REQS"
+    cat $PLATFORM_REQS > linux-platform
+else
+    echo "Using default"
+    cat "requirements-default.txt" > linux-platform
+fi
+
+stored_platform_reqs=$(<linux-platform)
+
+export linux_cmd_python_venv = '${python_cmd} -m pip install -r requirements-global.txt -r $stored_platform_reqs'
+export linux_cmd_conda_venv = '${python_cmd} -m pip install -r requirements-global.txt -r $stored_platform_reqs'
 
 /bin/bash ./linux-python-env.sh
--- update.sh
+++ update.sh
@@ -2,7 +2,9 @@
 
 source ./linux-env-vars.sh
 
-export linux_cmd_python_venv = 'git pull && ${python_cmd} -m pip install -r requirements-global.txt -r $PLATFORM_REQS --force-reinstall'
-export linux_cmd_conda_venv = 'git pull; ${python_cmd} -m pip install -r requirements-global.txt -r $PLATFORM_REQS" --force-reinstall)'
+stored_platform_reqs=$(<linux-platform)
+
+export linux_cmd_python_venv = 'git pull && ${python_cmd} -m pip install -r requirements-global.txt -r $stored_platform_reqs --force-reinstall'
+export linux_cmd_conda_venv = 'git pull; ${python_cmd} -m pip install -r requirements-global.txt -r $stored_platform_reqs --force-reinstall)'
 
 /bin/bash ./linux-python-env.sh
