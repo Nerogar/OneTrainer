82726b47eaeeb50e3c5a019f043a7ad4e35e79e0
Lolzen <funer@gmx.at>
2024-09-22 10:01:12 +0200

    fixup script derps

--- install.sh
+++ install.sh
@@ -39,21 +39,19 @@ ask() {
 }
 
 #clear linux-platform (used to store the selected platform-requirements, so we don't have to confirm on every update)
-> linux-platform
+> linux-platform.txt
 
 echo ""
 echo "Platform requirements diverge slightly depending on you GPU.Would you want to use the detected platform Requirements? ($PLATFORM_REQS)"
 if ask "Use $PLATFORM_REQS? default: Yes" Y; then
     echo "Using $PLATFORM_REQS"
-    cat $PLATFORM_REQS > linux-platform
+    echo $PLATFORM_REQS > linux-platform.txt
 else
     echo "Using default"
-    cat "requirements-default.txt" > linux-platform
+    echo "requirements-default.txt" > linux-platform.txt
 fi
 
-stored_platform_reqs=$(<linux-platform)
-
-export linux_cmd_python_venv='${python_cmd} -m pip install -r requirements-global.txt -r $stored_platform_reqs'
-export linux_cmd_conda_venv='${python_cmd} -m pip install -r requirements-global.txt -r $stored_platform_reqs'
+export linux_cmd_python_venv='${python_cmd} -m pip install -r requirements-global.txt -r $(<linux-platform.txt)'
+export linux_cmd_conda_venv='${python_cmd} -m pip install -r requirements-global.txt -r $(<linux-platform.txt)'
 
 /bin/bash ./linux-python-env.sh
--- update.sh
+++ update.sh
@@ -2,9 +2,7 @@
 
 source ./linux-env-vars.sh
 
-stored_platform_reqs=$(<linux-platform)
-
-export linux_cmd_python_venv='git pull && ${python_cmd} -m pip install -r requirements-global.txt -r $stored_platform_reqs --force-reinstall'
-export linux_cmd_conda_venv='git pull; ${python_cmd} -m pip install -r requirements-global.txt -r $stored_platform_reqs --force-reinstall)'
+export linux_cmd_python_venv='git pull && ${python_cmd} -m pip install -r requirements-global.txt -r $(<linux-platform.txt) --force-reinstall'
+export linux_cmd_conda_venv='git pull; ${python_cmd} -m pip install -r requirements-global.txt -r $(<linux-platform.txt) --force-reinstall)'
 
 /bin/bash ./linux-python-env.sh
