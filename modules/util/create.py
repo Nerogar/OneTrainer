fffc2e5edf86cd9671229eaf6d86962202261c7d
tikhonlavrev <101032353+tikhonlavrev@users.noreply.github.com>
2024-10-26 17:46:37 +0700

    AdEMAMix 8 bit fix and Adding full AdEMAMix support (#522)

--- modules/util/create.py
+++ modules/util/create.py
@@ -514,9 +514,26 @@ def create_optimizer(
                 betas=(optimizer_config.beta1 if optimizer_config.beta1 is not None else 0.9,
                        optimizer_config.beta2 if optimizer_config.beta2 is not None else 0.999,
                        optimizer_config.beta3 if optimizer_config.beta1 is not None else 0.9999,),
-                weight_decay=optimizer_config.weight_decay if optimizer_config.weight_decay is not None else 0.05,
+                weight_decay=optimizer_config.weight_decay if optimizer_config.weight_decay is not None else 1e-2,
                 eps=optimizer_config.eps if optimizer_config.eps is not None else 1e-8,
-                alpha=optimizer_config.eps if optimizer_config.eps is not None else 5,
+                alpha=optimizer_config.alpha if optimizer_config.alpha is not None else 5,
+                min_8bit_size=optimizer_config.min_8bit_size if optimizer_config.min_8bit_size is not None else 4096,
+                is_paged=optimizer_config.is_paged if optimizer_config.is_paged is not None else False,
+            )
+
+        # AdEMAMix Optimizer
+        case Optimizer.AdEMAMix:
+            import bitsandbytes as bnb
+            optimizer = bnb.optim.AdEMAMix(
+                params=parameters,
+                lr=config.learning_rate,
+                betas=(optimizer_config.beta1 if optimizer_config.beta1 is not None else 0.9,
+                       optimizer_config.beta2 if optimizer_config.beta2 is not None else 0.999,
+                       optimizer_config.beta3 if optimizer_config.beta1 is not None else 0.9999,),
+                weight_decay=optimizer_config.weight_decay if optimizer_config.weight_decay is not None else 1e-2,
+                eps=optimizer_config.eps if optimizer_config.eps is not None else 1e-8,
+                alpha=optimizer_config.alpha if optimizer_config.alpha is not None else 5,
+                optim_bits=optimizer_config.optim_bits if optimizer_config.optim_bits is not None else 32,
                 min_8bit_size=optimizer_config.min_8bit_size if optimizer_config.min_8bit_size is not None else 4096,
                 is_paged=optimizer_config.is_paged if optimizer_config.is_paged is not None else False,
             )
