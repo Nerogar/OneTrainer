9bbdc4ae997a2cce05ca5a4509ce96b554221e5a
Nerogar <nerogar@arcor.de>
2024-12-03 20:01:11 +0100

    fix a wrong concat order for flux fill training

--- modules/modelSetup/BaseFluxSetup.py
+++ modules/modelSetup/BaseFluxSetup.py
@@ -437,9 +437,8 @@ class BaseFluxSetup(
             )
 
             if config.model_type.has_mask_input() and config.model_type.has_conditioning_image_input():
-                latent_mask = batch['latent_mask']
                 latent_input = torch.concat(
-                    [scaled_noisy_latent_image, latent_mask, scaled_latent_conditioning_image], 1
+                    [scaled_noisy_latent_image, scaled_latent_conditioning_image, batch['latent_mask']], 1
                 )
             else:
                 latent_input = scaled_noisy_latent_image
@@ -469,13 +468,6 @@ class BaseFluxSetup(
                 latent_input.shape[2],
                 latent_input.shape[3],
             )
-
-            if config.model_type.has_mask_input() and config.model_type.has_conditioning_image_input():
-                latent_input = torch.concat(
-                    [scaled_noisy_latent_image, batch['latent_mask'], scaled_latent_conditioning_image], 1
-                )
-            else:
-                latent_input = scaled_noisy_latent_image
 
             packed_predicted_flow = model.transformer(
                 hidden_states=packed_latent_input.to(dtype=model.train_dtype.torch_dtype()),
