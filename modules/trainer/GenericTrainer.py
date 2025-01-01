d76a5bb85e792c04fb03f3d726841cef8396c1ec
dxqbYD <dxqbyd@outlook.de>
2025-01-01 17:50:52 +0100

    secrets.json with Hugging Face token (#619)

--- modules/trainer/GenericTrainer.py
+++ modules/trainer/GenericTrainer.py
@@ -37,6 +37,7 @@ from torch.utils.hooks import RemovableHandle
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms.functional import pil_to_tensor
 
+import huggingface_hub
 from PIL.Image import Image
 from tqdm import tqdm
 
@@ -120,6 +121,13 @@ class GenericTrainer(BaseTrainer):
                 print(f"Continuing training from backup '{last_backup_path}'...")
             else:
                 print("No backup found, continuing without backup...")
+
+        if self.config.secrets.huggingface_token != "":
+            self.callbacks.on_update_status("logging into Hugging Face")
+            huggingface_hub.login(
+                token = self.config.secrets.huggingface_token,
+                new_session = False,
+            )
 
         self.callbacks.on_update_status("loading the model")
         self.model = self.model_loader.load(
--- modules/ui/TopBar.py
+++ modules/ui/TopBar.py
@@ -3,8 +3,10 @@ import os
 import traceback
 import webbrowser
 from collections.abc import Callable
+from contextlib import suppress
 
 from modules.util import path_util
+from modules.util.config.SecretsConfig import SecretsConfig
 from modules.util.config.TrainConfig import TrainConfig
 from modules.util.enum.ModelType import ModelType
 from modules.util.enum.TrainingMethod import TrainingMethod
@@ -160,14 +162,24 @@ class TopBar:
     def __save_to_file(self, name) -> str:
         name = path_util.safe_filename(name)
         path = path_util.canonical_join("training_presets", f"{name}.json")
-        with open(path, "w") as f:
-            json.dump(self.train_config.to_dict(), f, indent=4)
 
+        config_dict = self.train_config.to_dict()
+        if 'secrets' in config_dict:
+            config_dict.pop('secrets')
+
+        with open(path, "w") as f:
+            json.dump(config_dict, f, indent=4)
+
+        return path
+
+    def __save_secrets(self, path) -> str:
+        with open(path, "w") as f:
+            json.dump(self.train_config.secrets.to_dict(), f, indent=4)
         return path
 
     def open_wiki(self):
         webbrowser.open("https://github.com/Nerogar/OneTrainer/wiki", new=0, autoraise=False)
-        
+
     def __save_new_config(self, name):
         path = self.__save_to_file(name)
 
@@ -210,6 +222,10 @@ class TopBar:
                     loaded_dict["__version"] = default_config.config_version
                 loaded_config = default_config.from_dict(loaded_dict).to_unpacked_config()
 
+            with suppress(FileNotFoundError), open("secrets.json", "r") as f:
+                secrets_dict=json.load(f)
+                loaded_config.secrets = SecretsConfig.default_values().from_dict(secrets_dict)
+
             self.train_config.from_dict(loaded_config.to_dict())
             self.ui_state.update(loaded_config)
 
@@ -228,3 +244,4 @@ class TopBar:
 
     def save_default(self):
         self.__save_to_file("#")
+        self.__save_secrets("secrets.json")
