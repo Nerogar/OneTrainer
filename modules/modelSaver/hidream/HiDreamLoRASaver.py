6042955b4412a7b36d2d4f7e8919ba584618482e
Nerogar <nerogar@arcor.de>
2025-05-01 17:05:45 +0200

    fix imports for HiDream lora conversions

--- modules/modelSaver/hidream/HiDreamLoRASaver.py
+++ modules/modelSaver/hidream/HiDreamLoRASaver.py
@@ -1,11 +1,12 @@
 from modules.model.HiDreamModel import HiDreamModel
 from modules.modelSaver.mixin.LoRASaverMixin import LoRASaverMixin
-from modules.util.convert.convert_lora_util import LoraConversionKeySet
-from modules.util.convert.lora.convert_hdiream_lora import convert_hidream_lora_key_sets
 from modules.util.enum.ModelFormat import ModelFormat
 
 import torch
 from torch import Tensor
+
+from omi_model_standards.convert.lora.convert_hidream_lora import convert_hidream_lora_key_sets
+from omi_model_standards.convert.lora.convert_lora_util import LoraConversionKeySet
 
 
 class HiDreamLoRASaver(
