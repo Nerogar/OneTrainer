Fix for off-by-one error with v_prediction (#965)
Fix for Off-by-one error with v_prediction aka rescale noise scheduler on UNet models

modules/modelSampler/FluxSampler.py
@@ -135,8 +135,10 @@ class FluxSampler(BaseModelSampler):
                 last_timestep = torch.ones(1, device=self.train_device, dtype=torch.int64) \
                                 * (noise_scheduler.config.num_train_timesteps - 1)
 
-                # add the final timestep to force predicting with zero snr
-                timesteps = torch.cat([last_timestep, timesteps])
+                # add the final timestep to force predicting with zero snr if it's not already here
+                if timesteps[0] != last_timestep:
+                    noise_scheduler.set_timesteps(diffusion_steps + 1, device=self.train_device)
+                    timesteps = torch.cat([last_timestep, timesteps])
 
             # denoising loop
             extra_step_kwargs = {}
@@ -398,8 +400,10 @@ class FluxSampler(BaseModelSampler):
                 last_timestep = torch.ones(1, device=self.train_device, dtype=torch.int64) \
                                 * (noise_scheduler.config.num_train_timesteps - 1)
 
-                # add the final timestep to force predicting with zero snr
-                timesteps = torch.cat([last_timestep, timesteps])
+                # add the final timestep to force predicting with zero snr if it's not already here
+                if timesteps[0] != last_timestep:
+                    noise_scheduler.set_timesteps(diffusion_steps + 1, device=self.train_device)
+                    timesteps = torch.cat([last_timestep, timesteps])
 
             # denoising loop
             extra_step_kwargs = {}

modules/modelSampler/PixArtAlphaSampler.py
@@ -91,8 +91,10 @@ class PixArtAlphaSampler(BaseModelSampler):
                 last_timestep = torch.ones(1, device=self.train_device, dtype=torch.int64) \
                                 * (noise_scheduler.config.num_train_timesteps - 1)
 
-                # add the final timestep to force predicting with zero snr
-                timesteps = torch.cat([last_timestep, timesteps])
+                # add the final timestep to force predicting with zero snr if it's not already here
+                if timesteps[0] != last_timestep:
+                    noise_scheduler.set_timesteps(diffusion_steps + 1, device=self.train_device)
+                    timesteps = torch.cat([last_timestep, timesteps])
 
             # prepare latent image
             num_channels_latents = transformer.config.in_channels