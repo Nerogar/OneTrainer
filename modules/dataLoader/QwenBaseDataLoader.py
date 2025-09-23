3ac4b879710935c147a54226ad938775cfc876c7
dxqb <dxqbYD@outlook.de>
2025-09-23 21:15:52 +0200

    Chroma, Qwen

--- modules/dataLoader/QwenBaseDataLoader.py
+++ modules/dataLoader/QwenBaseDataLoader.py
@@ -52,6 +52,7 @@ class QwenBaseDataLoader(
         if is_validation:
             config = copy.copy(config)
             config.batch_size = 1
+            config.multi_gpu = False
 
         self.__ds = self.create_dataset(
             config=config,
