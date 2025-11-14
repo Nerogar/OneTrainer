46d0f1a344a61ae96605e7a152237085c693a88e
Koratahiu~ <haidermontage191@gmail.com>
2025-11-14 07:41:00 +0300

    initial SpeeD

--- modules/util/enum/TimestepDistribution.py
+++ modules/util/enum/TimestepDistribution.py
@@ -8,6 +8,7 @@ class TimestepDistribution(Enum):
     HEAVY_TAIL = 'HEAVY_TAIL'
     COS_MAP = 'COS_MAP'
     INVERTED_PARABOLA = 'INVERTED_PARABOLA'
+    SPEED = 'SPEED'
 
     def __str__(self):
         return self.value
