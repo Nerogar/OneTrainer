dc900cb1eff9c4f15b2e83d0ecd533625e3bd1b9
O-J1 <18110006+O-J1@users.noreply.github.com>
2025-11-21 03:32:52 +1100

    Make all the enums uppercase

--- modules/util/enum/TensorboardMode.py
+++ modules/util/enum/TensorboardMode.py
@@ -2,6 +2,6 @@ from enum import StrEnum
 
 
 class TensorboardMode(StrEnum):
-    OFF = "off"
-    TRAIN_ONLY = "train_only"
-    ALWAYS_ON = "always_on"
+    OFF = "OFF"
+    TRAIN_ONLY = "TRAIN_ONLY"
+    ALWAYS_ON = "ALWAYS_ON"
