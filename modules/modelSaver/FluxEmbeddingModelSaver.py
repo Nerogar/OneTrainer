5994d11a15bb4cec3e735ba89442f197b3dece77
dxqb <183307934+dxqb@users.noreply.github.com>
2026-01-24 06:46:02 +0100

    generic model saver (#1210)

--- modules/modelSaver/FluxEmbeddingModelSaver.py
+++ modules/modelSaver/FluxEmbeddingModelSaver.py
@@ -1,32 +1,8 @@
 from modules.model.FluxModel import FluxModel
-from modules.modelSaver.BaseModelSaver import BaseModelSaver
 from modules.modelSaver.flux.FluxEmbeddingSaver import FluxEmbeddingSaver
-from modules.modelSaver.mixin.InternalModelSaverMixin import InternalModelSaverMixin
-from modules.util.enum.ModelFormat import ModelFormat
-from modules.util.enum.ModelType import ModelType
+from modules.modelSaver.GenericEmbeddingModelSaver import make_embedding_model_saver
 
-import torch
-
-
-class FluxEmbeddingModelSaver(
-    BaseModelSaver,
-    InternalModelSaverMixin,
-):
-    def __init__(self):
-        super().__init__()
-
-    def save(
-            self,
-            model: FluxModel,
-            model_type: ModelType,
-            output_model_format: ModelFormat,
-            output_model_destination: str,
-            dtype: torch.dtype | None,
-    ):
-        embedding_model_saver = FluxEmbeddingSaver()
-
-        embedding_model_saver.save_multiple(model, output_model_format, output_model_destination, dtype)
-        embedding_model_saver.save_single(model, output_model_format, output_model_destination, dtype)
-
-        if output_model_format == ModelFormat.INTERNAL:
-            self._save_internal_data(model, output_model_destination)
+FluxEmbeddingModelSaver = make_embedding_model_saver(
+    model_class=FluxModel,
+    embedding_saver_class=FluxEmbeddingSaver,
+)
--- modules/modelSaver/StableDiffusionFineTuneModelSaver.py
+++ modules/modelSaver/StableDiffusionFineTuneModelSaver.py
@@ -1,34 +1,10 @@
 from modules.model.StableDiffusionModel import StableDiffusionModel
-from modules.modelSaver.BaseModelSaver import BaseModelSaver
-from modules.modelSaver.mixin.InternalModelSaverMixin import InternalModelSaverMixin
+from modules.modelSaver.GenericFineTuneModelSaver import make_fine_tune_model_saver
 from modules.modelSaver.stableDiffusion.StableDiffusionEmbeddingSaver import StableDiffusionEmbeddingSaver
 from modules.modelSaver.stableDiffusion.StableDiffusionModelSaver import StableDiffusionModelSaver
-from modules.util.enum.ModelFormat import ModelFormat
-from modules.util.enum.ModelType import ModelType
 
-import torch
-
-
-class StableDiffusionFineTuneModelSaver(
-    BaseModelSaver,
-    InternalModelSaverMixin,
-):
-    def __init__(self):
-        super().__init__()
-
-    def save(
-            self,
-            model: StableDiffusionModel,
-            model_type: ModelType,
-            output_model_format: ModelFormat,
-            output_model_destination: str,
-            dtype: torch.dtype | None,
-    ):
-        base_model_saver = StableDiffusionModelSaver()
-        embedding_model_saver = StableDiffusionEmbeddingSaver()
-
-        base_model_saver.save(model, model_type, output_model_format, output_model_destination, dtype)
-        embedding_model_saver.save_multiple(model, output_model_format, output_model_destination, dtype)
-
-        if output_model_format == ModelFormat.INTERNAL:
-            self._save_internal_data(model, output_model_destination)
+StableDiffusionFineTuneModelSaver = make_fine_tune_model_saver(
+    model_class=StableDiffusionModel,
+    model_saver_class=StableDiffusionModelSaver,
+    embedding_saver_class=StableDiffusionEmbeddingSaver,
+)
