d1fec84afb9c6276eec55066783e96c42811a40f
krigeta <75309361+krigeta@users.noreply.github.com>
2026-02-18 17:08:15 +0530

    Add argument parsing for host and port in WebUI

--- scripts/train_webui.py
+++ scripts/train_webui.py
@@ -2,16 +2,23 @@ from util.import_util import script_imports
 
 script_imports()
 
+import argparse
+
 import gradio as gr
 
 from modules.webui.app import create_app, LOGO_DIR
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Launch OneTrainer WebUI")
+    parser.add_argument("--host", default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=7860)
+    args = parser.parse_args()
+
     app = create_app()
     app.launch(
-        server_name="0.0.0.0",
-        server_port=7860,
+        server_name=args.host,
+        server_port=args.port,
         share=True,
         theme=gr.themes.Soft(),
         allowed_paths=[LOGO_DIR],
