afe6b42301615687ff80fc6353666e2cecbde9ab
O-J1 <18110006+O-J1@users.noreply.github.com>
2026-03-10 18:15:35 +1100

    Fix run-cmd.ps1 syntax

--- run-cmd.ps1
+++ run-cmd.ps1
@@ -1,5 +1,20 @@
-. "$PSScriptRoot\lib.include.ps1"
+# Xet is buggy. Disabled by default unless already defined - https://github.com/Nerogar/OneTrainer/issues/949
+if (-not $env:HF_HUB_DISABLE_XET) { $env:HF_HUB_DISABLE_XET = "1" }
+
+. "$PSScriptRoot\scripts\powershell\lib.include.ps1"
+
+if ($args.Length -eq 0) {
+    Write-OTError 'You must provide the name of the script to execute, such as "train".'
+    exit 1
+}
+
+$OT_CUSTOM_SCRIPT_FILE = "scripts/$($args[0]).py"
+if (-not (Test-Path $OT_CUSTOM_SCRIPT_FILE)) {
+    Write-OTError "Custom script file `"$OT_CUSTOM_SCRIPT_FILE`" does not exist."
+    exit 1
+}
 
 Prepare-RuntimeEnvironment
 
-Invoke-InEnv python "scripts/@args[1].py" @args[1..]
+$remainingArgs = if ($args.Length -gt 1) { $args[1..($args.Length - 1)] } else { @() }
+Invoke-InEnv python $OT_CUSTOM_SCRIPT_FILE @remainingArgs
