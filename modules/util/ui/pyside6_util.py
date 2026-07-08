b36f9512477f2658172016f76dc69f59b31add5a
Zueuk <zueuk@users.noreply.github.com>
2026-07-08 20:36:00 +0300

    Styled checkbox

--- modules/util/ui/pyside6_util.py
+++ modules/util/ui/pyside6_util.py
@@ -29,28 +29,34 @@ def create_application() -> QApplication:
         Qt.ColorScheme.Light: {
             "base": "#DBDBDB",
             "text": "#1C1C1C",
-            "disabled_base": "#F5F5F5",
+            "disabled": "#F5F5F5",
             "window": "#CFCFCF",
             "window_text": "#1C1C1C",
-            "button": "#3B8ED0",
-            "button_hover": "#36719F",
+            "button": "#36719F",
+            "button_hover": "#3B8ED0",
             "button_disabled": "#A0A0A0",
             "editbox": "#F9F9F9",
             "editbox_frame": "#999CA1",
             "progress_bar": "#939BA2",
+            "checkbox": "#5A8CB2",
+            "checkbox_hover": "#6AA5D2",
+            "checkbox_glyph": "checkbox_light.png",
         },
         Qt.ColorScheme.Dark: {
             "base": "#2B2B2B",
             "text": "#DCE4EE",
-            "disabled_base": "#2D2D2D",
+            "disabled": "#2D2D2D",
             "window": "#333333",
             "window_text": "#DCE4EE",
-            "button": "#1F6AA5",
-            "button_hover": "#144870",
+            "button": "#144870",
+            "button_hover": "#195A8C",
             "button_disabled": "#404040",
             "editbox": "#343638",
             "editbox_frame": "#565B5E",
             "progress_bar": "#4A4D50",
+            "checkbox": "#346185",
+            "checkbox_hover": "#3D739C",
+            "checkbox_glyph": "checkbox_dark.png",
         },
     }
 
@@ -59,7 +65,7 @@ def create_application() -> QApplication:
         colors = color_schemes[scheme]
         palette.setColor(QPalette.ColorRole.Base, QColor(colors["base"]))
         palette.setColor(QPalette.ColorRole.Text, QColor(colors["text"]))
-        palette.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole.Base, QColor(colors["disabled_base"]))
+        palette.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole.Base, QColor(colors["disabled"]))
         palette.setColor(QPalette.ColorRole.Window, QColor(colors["window"]))
         palette.setColor(QPalette.ColorRole.WindowText, QColor(colors["window_text"]))
         app.setPalette(palette)
@@ -74,6 +80,29 @@ def create_application() -> QApplication:
             QCheckBox::indicator {{
                 width: 16px;
                 height: 16px;
+                background-color: transparent;
+                border: 1px solid {colors["editbox_frame"]};
+                border-radius: 4px;
+            }}
+            QCheckBox::indicator:hover {{
+                border-color: {colors["checkbox_hover"]};
+            }}
+            QCheckBox::indicator:checked {{
+                image: url(resources/icons/{colors["checkbox_glyph"]});
+                background-color: {colors["checkbox"]};
+                border: 1px solid {colors["checkbox"]};
+            }}
+            QCheckBox::indicator:checked:hover {{
+                background-color: {colors["checkbox_hover"]};
+                border: 1px solid {colors["checkbox_hover"]};
+            }}
+            QCheckBox::indicator:disabled {{
+                background-color: {colors["disabled"]};
+                border-color: {colors["button_disabled"]};
+            }}
+            QCheckBox::indicator:checked:disabled {{
+                background-color: {colors["disabled"]};
+                border-color: {colors["button_disabled"]};
             }}
             QProgressBar {{
                 background-color: {colors["progress_bar"]};
