decc286bd67c8651867b22872279ac091998a71b
Zueuk <zueuk@users.noreply.github.com>
2026-07-11 13:20:12 +0300

    Scrollable LoRA tab for consistent look

--- modules/ui/PySide6LoraTabView.py
+++ modules/ui/PySide6LoraTabView.py
@@ -3,7 +3,7 @@ from modules.ui.LoraTabController import LoraTabController
 from modules.util.enum.ModelType import PeftType
 from modules.util.ui import pyside6_components
 
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QScrollArea, QWidget
 
 
 class PySide6LoraTabView(BaseLoraTabView, QWidget):
@@ -16,6 +16,7 @@ class PySide6LoraTabView(BaseLoraTabView, QWidget):
         self.controller = controller
         self.ui_state = ui_state
         self.scroll_frame = None
+        self.inner_frame = None
         self.options_frame = None
         self.refresh_ui()
 
@@ -24,22 +25,27 @@ class PySide6LoraTabView(BaseLoraTabView, QWidget):
             self.scroll_frame.hide()
             self.scroll_frame.deleteLater()
 
-        self.scroll_frame = QWidget(self)
+        self.scroll_frame = QScrollArea(self)
+        self.scroll_frame.setWidgetResizable(True)
         pyside6_components._layout(self).addWidget(self.scroll_frame, 0, 0)
-        lo = pyside6_components._layout(self.scroll_frame)
+
+        self.inner_frame = QWidget()
+        self.scroll_frame.setWidget(self.inner_frame)
+
+        lo = pyside6_components._layout(self.inner_frame)
         lo.setContentsMargins(pyside6_components.PAD, pyside6_components.PAD, pyside6_components.PAD, pyside6_components.PAD)
         lo.setColumnStretch(1, 1)
         lo.setColumnStretch(2, 2)
-        self.build(self.scroll_frame, self.controller, self.ui_state, self.setup_lora)
-        pyside6_components._pack_form(self.scroll_frame)
+        self.build(self.inner_frame, self.controller, self.ui_state, self.setup_lora)
+        pyside6_components._pack_form(self.inner_frame)
 
     def setup_lora(self, peft_type: PeftType):
         if self.options_frame is not None:
             self.options_frame.hide()
             self.options_frame.deleteLater()
 
-        self.options_frame = QWidget(self.scroll_frame)
-        pyside6_components._layout(self.scroll_frame).addWidget(self.options_frame, 1, 0, 1, 3)
+        self.options_frame = QWidget(self.inner_frame)
+        pyside6_components._layout(self.inner_frame).addWidget(self.options_frame, 1, 0, 1, 3)
         lo = pyside6_components._layout(self.options_frame)
         lo.setColumnStretch(1, 1)
         lo.setColumnStretch(4, 1)
